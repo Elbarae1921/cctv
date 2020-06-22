fe31bcdbaad19a54a63290850faf8173f9c2f9c7
Elbarae <elbarae1921@gmail.com>
2020-06-22 15:54:49 +0100

    Added pause functionality

--- src/public/script.js
+++ src/public/script.js
@@ -7,11 +7,17 @@ const frames = [];
 // boolean to check if the frames array is empty
 let load = true;
 
+// initialize the isPaused variable for pause functionality
+let isPaused = false;
+
 // get the image element
 const image = document.getElementById("image");
 
 // get the loading spinner
 const loading = document.getElementById("loading");
+
+// get the pause icon div
+const pause = document.getElementById("pause");
 
 // connect to the socket server
 const socket = io.connect("http://localhost:5000");
@@ -28,22 +34,41 @@ socket.on("image", (image64) => {
     frames.push(image64);
 });
 
+// image pause onclick
+image.onclick = () => {
+    isPaused = !isPaused;
+    pause.style.display = isPaused ? "flex" : "none";
+};
+
+// pause onclick event since the image would be hidden
+pause.onclick = () => {
+    isPaused = !isPaused;
+    pause.style.display = isPaused ? "flex" : "none";
+};
+
+// function to display frames at a 1000/FPS frame per second ratio
 setInterval(() => {
-    if(load) {
-        if(frames.length >= FPS) {
-            loading.style.display = "none";
-            image.src = `data:image/jpeg;base64,${frames.shift()}`;
-            load = false;
-        }
-    }
-    else {
-        if(frames.length != 0) {
-            loading.style.display = "none";
-            image.src = `data:image/jpeg;base64,${frames.shift()}`;
+    // check if is paused
+    if(!isPaused) {
+        // check if the frames array was empty the last time
+        if(load) {
+            // check if the frames array contains enough frames, otherwise wait for it to fill
+            if(frames.length >= FPS) {
+                loading.style.display = "none";
+                image.src = `data:image/jpeg;base64,${frames.shift()}`;
+                load = false;
+            }
         }
         else {
-            loading.style.display = "flex";
-            load = true;
+            // check if the frames array is not empty
+            if(frames.length != 0) {
+                loading.style.display = "none";
+                image.src = `data:image/jpeg;base64,${frames.shift()}`;
+            }
+            else {
+                loading.style.display = "flex";
+                load = true;
+            }
         }
-    }    
+    }       
 }, 1000 / FPS);
